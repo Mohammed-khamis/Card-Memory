e8d8ee14cfe8563bc6bf509997f8ffc43498abbf
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-09-17 20:40:46 +0300

    todo List

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
-const todoAdd = document.querySelector("main #todo");
+const todoList = document.querySelector("main #todo ul")
 
-const inProgressAdd = document.querySelector("main #in_progress");
+const progressList = document.querySelector("main #in_progress ul")
 
-const doneAdd = document.querySelector("main #done");
+const doneList = document.querySelector("main #done ul")
 
-const todoList = document.createElement("ul");
+let liTodo;
+let liProgress;
+let liDone;
 
-const progressList = document.createElement("ul");
-
-const doneList = document.createElement("ul");
-
-todoAdd.append(todoList);
-inProgressAdd.append(progressList);
-doneAdd.append(doneList);
+const todoArray = [];
+const progressArray = [];
+const doneArray = [];
 
 const add = function () {
     const action = prompt("Enter your activity, please?");
     if (action !== "") {
-        const element = document.createElement("li");
-        element.innerText = action;
+        const todo = document.createElement("li");
+        todo.innerText = action;
         const addToProgress = document.createElement("button");
         addToProgress.innerText = "Move to in-progress list";
         const addToDone = document.createElement("button");
         addToDone.innerText = "Move to done list";
-        todoList.append(element);
-        element.append(addToProgress)
+        todoList.append(todo);
+        todo.append(addToProgress)
+        liTodo = todo;
+        todoArray.push(liTodo);
         
         addToProgress.onclick = function () {
-            element.innerText = action;
+            todo.innerText = action;
             const progress = document.createElement("li");
-            progress.innerText = element.innerText;
+            progress.innerText = todo.innerText;
             progressList.append(progress);
             progress.append(addToDone);
-            element.remove();
+            todo.remove();
             addToProgress.remove();
+            liProgress = progress;
+            progressArray.push(liProgress);
 
             addToDone.onclick = function () {
                 progress.innerText = action;
@@ -42,6 +44,8 @@ const add = function () {
                 doneList.append(done);
                 progress.remove();
                 addToDone.remove();
+                liDone = done;
+                doneArray.push(liDone);
             };
         
         };
@@ -49,25 +53,22 @@ const add = function () {
 };
 
 const reset = function () {
-    doneList.remove();
-    progressList.remove();
-    todoList.remove();
-    return function () {
-        const todoAdd = document.querySelector("main #todo");
-    
-        const inProgressAdd = document.querySelector("main #in_progress");
-    
-        const doneAdd = document.querySelector("main #done");
-    
-        const todoList = document.createElement("ul");
-    
-        const progressList = document.createElement("ul");
-    
-        const doneList = document.createElement("ul");
-    
-        todoAdd.append(todoList);
-        inProgressAdd.append(progressList);
-        doneAdd.append(doneList);
-    };
+    if (document.querySelector("main #todo ul li")) {
+        for (let i = 0; i < todoArray.length; i++) {
+            liTodo = todoArray[i];
+            liTodo.remove();
+        }
+    }
+    if (document.querySelector("main #in_progress ul li")) {
+        for (let i = 0; i < progressArray.length; i++) {
+            liProgress = progressArray[i];
+            liProgress.remove();
+        }
+    }
+    if (document.querySelector("main #done ul li")) {
+        for (let i = 0; i < doneArray.length; i++) {
+            liDone = doneArray[i];
+            liDone.remove();
+        }
+    }
 };
-
